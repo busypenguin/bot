046a571fd2ccd12996ee3b8608530b28b583e64d
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-09-20 20:09:40 +0300

    add DialogFlow learning script.

---
+++ load_intent.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+
+load_dotenv()
+credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+project_id = os.getenv('PROJECT_ID')
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+response = requests.get('https://dvmn.org/media/filer_public/a7/db/a7db66c0-1259-4dac-9726-2d1fa9c44f20/questions.json')
+intents = response.json()
+for intent in intents:
+    create_intent(project_id, intent, intents[intent]['questions'], [intents[intent]['answer'],])
