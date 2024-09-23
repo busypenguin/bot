77949da592ef01e6a75cd2a09694c5d63b64652c
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-09-23 15:32:31 +0300

    bot is silent, if it does not understand user

--- vk_bot.py
+++ vk_bot.py
@@ -10,10 +10,6 @@ load_dotenv()
 vk_token = os.getenv('VK_TOKEN')
 project_id = os.getenv('PROJECT_ID')
 
-vk_session = vk.VkApi(token=vk_token)
-
-longpoll = VkLongPoll(vk_session)
-
 
 def detect_intent_texts(project_id, session_id, text, language_code):
     """Returns the result of detect intent with texts as inputs.
@@ -33,16 +29,19 @@ def detect_intent_texts(project_id, session_id, text, language_code):
         request={"session": session, "query_input": query_input}
     )
 
-    return response.query_result.fulfillment_text
+    return response
 
 
 def get_answer(event, vk_api):
     """Answer the user message."""
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=(detect_intent_texts(project_id, event.user_id, event.text, 'ru-RU')),
-        random_id=random.randint(1,1000)
-    )
+    response = detect_intent_texts(project_id, event.user_id, event.text, 'ru-RU')
+    if not response.query_result.intent.is_fallback:
+        answer = response.query_result.fulfillment_text
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=answer,
+            random_id=random.randint(1, 1000)
+        )
 
 
 if __name__ == "__main__":
